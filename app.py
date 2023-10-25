02a123d6656848299c0007128ac01471be724a61
lilachdavis <lilacd@gmail.com>
2023-10-25 12:22:29 +0100

    add cards

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
 import os
 from flask import Flask, request, render_template
 from lib.database_connection import get_flask_database_connection
+from lib.space_repo import SpaceRepository
+from lib.space import Space
 
-# Create a new Flask app
+
 app = Flask(__name__)
 
-# == Your Routes Here ==
+# Routes
 
-# GET /index
-# Returns the homepage
-# Try it:
-#   ; open http://localhost:5000/index
 @app.route('/index', methods=['GET'])
 def get_index():
     return render_template('/index.html')
 
 @app.route('/users/new', methods=['GET'])
 def get_new_user():
-    return render_template("/users/create.html")
+    return render_template('/users/create.html')
 
 @app.route('/spaces/list')
 def space_list():
-    return render_template('/spaces/list.html')
+    connection = get_flask_database_connection(app)
+    repo = SpaceRepository(connection)
+    spaces = repo.all()
+    return render_template('/spaces/list.html', spaces=spaces)
 
 
-# These lines start the server if you run this file directly
-# They also start the server configured to use the test database
-# if started in test mode.
+
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
