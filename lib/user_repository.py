0c02faca35cf50f21b3aa807a482677b43ab123d
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-10-25 11:04:14 +0100

    user login methods added

--- lib/user_repository.py
+++ lib/user_repository.py
@@ -1,5 +1,6 @@
 from lib.user import User
 from lib.base_repository_class import BaseModelManager
+import hashlib
 
 
 class UserRepository(BaseModelManager):
@@ -8,13 +9,32 @@ class UserRepository(BaseModelManager):
         self._model_class = User
         self._table_name = 'users'
 
-    def create(self, user):
+    def create(self, email, password):
+        #hash the password
+        binary_password = password.encode("utf-8")
+        hashed_password = hashlib.sha256(binary_password).hexdigest()
+
         rows = self._connection.execute(
-            'INSERT INTO users (email, username, password) VALUES (%s, %s, %s) RETURNING id',
-            [user.email, user.username, user.password]
+            'INSERT INTO users (email, password) VALUES (%s, %s) RETURNING id',
+            [email, hashed_password]
         )
-        user.id = rows[0].get('id')
-        return user
+        # user.id = rows[0].get('id')
+        # return user
+
+    def check_password(self, email, password_attempt):
+        # hash the password attempt
+        binary_password_attempt = password_attempt.encode("utf-8")
+        hashed_password_attempt = hashlib.sha256(binary_password_attempt).hexdigest()
+
+        # Check whether there is a user in the database with the given email
+        # and a matching password hash, using a SELECT statement.
+        rows = self._connection.execute(
+            'SELECT * FROM users WHERE email = %s AND password = %s',
+            [email, hashed_password_attempt]
+        )
+
+        # If that SELECT finds any rows, the password is correct.
+        return len(rows) > 0
 
     #TODO: Move it to BASE CLASS
     def update(self, user):
