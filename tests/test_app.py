3e3b1c910db3bcf99d46dfd59e09162fa11cde62
Aigul <aigul.djon@gmail.com>
2023-10-25 17:01:22 +0100

    Add missing tests for space pages

--- tests/test_app.py
+++ tests/test_app.py
@@ -24,7 +24,7 @@ def test_link_create_user(page, test_web_address):
 
     expect(h5_tag).to_have_text("Create New User")
 
-def test_detail_vew(page, test_web_address):
+def test_detail_view(page, test_web_address):
     page.goto(f"http://{test_web_address}/spaces/detail/1")
     name_tag = page.locator('.card-title')
     expect(name_tag).to_have_text('MYPLACE1 £10.0')
@@ -32,3 +32,28 @@ def test_detail_vew(page, test_web_address):
     dates = page.locator('.list-group-item')
     expect(dates).to_have_text(['2023-10-29', '2023-10-30'])
     
+def test_list_view(page, test_web_address, db_connection):
+    db_connection.seed('seeds/makers_bnb_library.sql')
+
+    page.goto(f"http://{test_web_address}/spaces/list")
+    title_tag = page.locator('.card-title')
+    expect(title_tag).to_have_text(
+        ['MYPLACE1 £10.0', 'MYPLACE2 £15.0', 'MYPLACE3 £20.0', 'MYPLACE4 £30.0', 'MYPLACE5 £18.0']
+        )
+
+def test_list_spaces_by_user_id(page, test_web_address, db_connection):
+    db_connection.seed('seeds/makers_bnb_library.sql')
+
+    page.goto(f"http://{test_web_address}/users/1/spaces")
+    title_tag = page.locator('.card-title')
+    expect(title_tag).to_have_text(['MYPLACE1 £10.0', 'MYPLACE2 £15.0'])
+
+
+def test_see_more_button(page, test_web_address, db_connection):
+    db_connection.seed('seeds/makers_bnb_library.sql')
+
+    page.goto(f"http://{test_web_address}/spaces/list")
+    page.locator('.btn-primary').first    
+    page.click("text=See more")
+    title_tag = page.locator('.card-title')
+    expect(title_tag).to_have_text('MYPLACE1 £10.0')
